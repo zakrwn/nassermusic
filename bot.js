56528a5d63b316e78c86283f274430e5432a953d
zakrwn <45009413+zakrwn@users.noreply.github.com>
2018-11-23 13:07:10 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -342,6 +342,25 @@ client.on('message', message => {
     }
 });
 
+
+
+client.on('message', msg => {
+
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("ايدي الروم").join(); //by : iBeAnthonyD
+    });
+
+
+
 const devs = ["295909228328255488"]; //الايدي حقك
 const adminprefix = ["N"]; // البرفكس
 client.on('message', message => {
